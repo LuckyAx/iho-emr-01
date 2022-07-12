8b6077298cad80457009651cc078f020efe94061
qianhaibin <qianhaibin@cnhis.com>
2022-07-12 10:24:37 +0800

    新增项目

--- emr-demo/src/main/java/com/cnhis/emr/rest/EmrDemoApplication.java
+++ emr-demo/src/main/java/com/cnhis/emr/rest/EmrDemoApplication.java
@@ -3,6 +3,9 @@ package com.cnhis.emr.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
 @SpringBootApplication
 public class EmrDemoApplication {
 
