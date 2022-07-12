cd4fc5130cc34ffd5114a664066e6fc3375310a7
qianhaibin <qianhaibin@cnhis.com>
2022-07-12 10:25:26 +0800

    修改代码

--- emr-demo/src/main/java/com/cnhis/emr/rest/EmrDemoApplication.java
+++ emr-demo/src/main/java/com/cnhis/emr/rest/EmrDemoApplication.java
@@ -3,9 +3,6 @@ package com.cnhis.emr.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-
 @SpringBootApplication
 public class EmrDemoApplication {
 
